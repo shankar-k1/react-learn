2f9bae9e7425e810e2a5336c6f8b50f481c4be20
shankar-k1 <shan68973@gmail.com>
2023-05-08 16:31:26 +0530

    modify commit to view the logs --Use git commit stats

--- Retouch Javascript/Script.js
+++ Retouch Javascript/Script.js
@@ -61,30 +61,3 @@ function StringCheck(){
     document.getElementById("namecheck").innerHTML=number;
     document.getElementById("namecheck").innerHTML=number1;
 }
-let carName1="Volvo XC60";
-undefined
-carName1
-'Volvo XC60'
-let x-10l
-let y=20
-undefined
-let num2=10
-undefined
-(num1==num2)
-true
-const cars=["Sarah","Volvo","Benz"]
-console.log(cars)
-const car=["Sarah","Volvo","Benz"]
-console.log(cars)
-VM706:2 (3) ['Saab', 'Volvo', 'BMW']
-undefined
-const car=["Sarah","Volvo","Benz"]/
-console.log(car)
-VM719:2 (3) ['Sarah', 'Volvo', 'Benz']0: "Sarah"1: "Volvo"2: "Benz"length: 3[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
-undefined
-typeof cars
-'object'
-let river
-undefined
-let river =""
-undefined
