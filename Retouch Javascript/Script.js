5f7058e2d610732ad6d4bb12735295335c6d250b
shankar-k1 <shan68973@gmail.com>
2023-05-08 15:34:57 +0530

    extra commit

---
+++ Retouch Javascript/Script.js
@@ -0,0 +1,90 @@
+function mycustomfunction(){
+    document.getElementById("demo").innerHTML="Hi Shankara";
+}
+function mycustomfunction1(){
+    document.getElementById("add").innerHTML=5+6;
+}
+document.write(5+6);
+window.alert("This is a testing site");
+alert("Ignore the previous pop-up and consider improvising the learning streaks daily")
+
+console.log("Almost started with javascript by touching the base concepts");
+
+
+//javascript print/
+
+
+/*this print()method is used to print objects and methos and we can use alt text in any of the html property
+window.print(name);
+*/
+
+
+//Statements
+let a,b,sum,sub,mul,div;
+a=6;
+b=10;
+
+sum=a&b;
+sub=a%b;
+mul=a>>b;
+div=a<<b;
+
+function myfunction(){
+    document.getElementById("demo").innerHTML="The solution for all the operators are 1.add:" + sum +"2.Sub"+sub+"3.Mul"+mul+"4.Div"+div;
+}
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+let number= 16+"Volvo";
+let number1= "16"+"VOlvo";
+
+function StringCheck(){
+    document.getElementById("namecheck").innerHTML=number;
+    document.getElementById("namecheck").innerHTML=number1;
+}
+let carName1="Volvo XC60";
+undefined
+carName1
+'Volvo XC60'
+let x-10l
+let y=20
+undefined
+let num2=10
+undefined
+(num1==num2)
+true
+const cars=["Sarah","Volvo","Benz"]
+console.log(cars)
+const car=["Sarah","Volvo","Benz"]
+console.log(cars)
+VM706:2 (3) ['Saab', 'Volvo', 'BMW']
+undefined
+const car=["Sarah","Volvo","Benz"]/
+console.log(car)
+VM719:2 (3) ['Sarah', 'Volvo', 'Benz']0: "Sarah"1: "Volvo"2: "Benz"length: 3[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+undefined
+typeof cars
+'object'
+let river
+undefined
+let river =""
+undefined
